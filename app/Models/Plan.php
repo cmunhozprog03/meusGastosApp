12aa5dfbd6780bc746ea0236efea8e4b8f0d18a6
cmunhozprog <cmunhozprog03@hotmail.com>
2021-01-27 17:17:46 -0300

    Módulo 01 Completo

--- app/Models/Plan.php
+++ app/Models/Plan.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Plan extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name', 'description', 'price', 'slug', 'reference'];
+
+    public function features()
+    {
+        return $this->hasMany(Feature::class);
+    }
 }
