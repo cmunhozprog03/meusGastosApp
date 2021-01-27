089caab0c60edc70500e5f60db12bad2d9909af4
cmunhozprog <cmunhozprog03@hotmail.com>
2021-01-27 19:32:08 -0300

    Lesson 15

--- app/Http/Livewire/Expense/ExpenseCreate.php
+++ app/Http/Livewire/Expense/ExpenseCreate.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Livewire\Expense;
 
+use App\Models\Expense;
 use Livewire\Component;
 
 class ExpenseCreate extends Component
@@ -9,6 +10,30 @@ class ExpenseCreate extends Component
     public $amount;
     public $description;
     public $type;
+
+
+    protected $rules = [
+        'amount' => 'required',
+        'type' => 'required',
+        'description' => 'required'
+    ];
+    
+
+    public function createExpense()
+    {
+        $this->validate();
+
+        Expense::create([
+            'amount' => $this->amount,
+            'description' => $this->description,
+            'type' => $this->type,
+            'user_id' => 1
+        ]);
+
+        session()->flash('message', 'Registro criado com sucesso!');
+
+        $this->amount = $this->description = $this->type = null;
+    }
     
 
     public function render()
--- resources/views/livewire/expense/expense-create.blade.php
+++ resources/views/livewire/expense/expense-create.blade.php
@@ -1,17 +1,30 @@
 <div class="ml-5">
+
+    @if (session()->has('message'))
+        <h3>{{session('message')}}</h3>
+    @endif
+
     <h3 class="text-blue-600 ml-5">Criar Registros</h3>
 
     <hr>
     
-    <form action="">
+    <form action="" wire:submit.prevent="createExpense">
         <p>
-            <label for="">Descrição</label>
+            <label for="" >Descrição</label>
             <input class="shadow border-t" type="text" name="Description" wire:model="description">
+            
+            @error('description')
+                <h5>{{$message}}</h5> 
+            @enderror
         </p>
         
         <p>
             <label for="">Valor</label>
             <input class="shadow border-t" type="text" name="amount" wire:model="amount">
+        
+            @error('description')
+                <h5>{{$message}}</h5> 
+            @enderror
         </p>
         
         <p>
@@ -21,6 +34,10 @@
                 <option value="1">Entrada</option>
                 <option value="2">Saída</option>
             </select>
+
+            @error('description')
+                <h5>{{$message}}</h5> 
+            @enderror
         </p>
         <button class="bg-green-400" type="submit">Criar Registro</button>
     </form>
